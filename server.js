b60064d76fe20aee9ccbfecf1084feff9f365c7f
Giulia Bonadio Lisboa <63248417+Giu731@users.noreply.github.com>
2020-04-15 11:40:25 -0300

    Add files via upload
    Creating a server and using template engine and nunjucks.

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+
+server.use(express.static('public'))
+
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server
+})
+
+
+
+server.get("/", function(req, res){
+    return res.render("content")
+})
+
+server.get("/description", function(req, res){
+    return res.render("description")
+})
+
+server.use(function(req, res) {
+    res.status(404).render("not-found")
+})
+
+
+server.listen(5000, function(){
+    console.log("server is running")
+})
